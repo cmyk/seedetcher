fda929c226c3c8dad3e068903f89ffb50999bea0
cmyk <schelob@protonmail.com>
2025-12-06 21:39:25 +0100

    refactor(gui): move warning/error dialogs

--- gui/gui.go
+++ gui/gui.go
@@ -9,17 +9,14 @@ import (
 	"image/draw"
 	"io"
 	"math"
-	"strings"
 	"time"
 
 	"github.com/btcsuite/btcd/btcutil/hdkeychain"
 	"seedetcher.com/bc/urtypes"
 	"seedetcher.com/bip32"
 	"seedetcher.com/bip39"
-	"seedetcher.com/gui/assets"
 	"seedetcher.com/gui/op"
 	"seedetcher.com/gui/saver"
-	"seedetcher.com/gui/widget"
 	"seedetcher.com/printer"
 )
 
@@ -216,170 +213,6 @@ func scaleRot(dst, src *image.Gray, rot180 bool) {
 	}
 }
 
-type ErrorScreen struct {
-	Title string
-	Body  string
-	w     Warning
-	inp   InputTracker
-}
-
-func (s *ErrorScreen) Layout(ctx *Context, ops op.Ctx, th *Colors, dims image.Point) bool {
-	for {
-		e, ok := s.inp.Next(ctx, Button3)
-		if !ok {
-			break
-		}
-		switch e.Button {
-		case Button3:
-			if s.inp.Clicked(e.Button) {
-				return true
-			}
-		}
-	}
-	s.w.Layout(ctx, ops, th, dims, s.Title, s.Body)
-	layoutNavigation(&s.inp, ops, th, dims, []NavButton{{Button: Button3, Style: StylePrimary, Icon: assets.IconCheckmark}}...)
-	return false
-}
-
-type ConfirmWarningScreen struct {
-	Title string
-	Body  string
-	Icon  image.RGBA64Image
-
-	warning Warning
-	confirm ConfirmDelay
-	inp     InputTracker
-}
-
-type Warning struct {
-	scroll  int
-	txtclip int
-	inp     InputTracker
-}
-
-type ConfirmResult int
-
-const (
-	ConfirmNone ConfirmResult = iota
-	ConfirmNo
-	ConfirmYes
-)
-
-type ConfirmDelay struct {
-	timeout time.Time
-}
-
-func (c *ConfirmDelay) Start(ctx *Context, delay time.Duration) {
-	c.timeout = ctx.Platform.Now().Add(delay)
-}
-
-func (c *ConfirmDelay) Progress(ctx *Context) float32 {
-	if c.timeout.IsZero() {
-		return 0.
-	}
-	now := ctx.Platform.Now()
-	d := c.timeout.Sub(now)
-	if d <= 0 {
-		return 1.
-	}
-	ctx.Platform.Wakeup()
-	return 1. - float32(d.Seconds()/confirmDelay.Seconds())
-}
-
-const confirmDelay = 1 * time.Second
-
-func (w *Warning) Layout(ctx *Context, ops op.Ctx, th *Colors, dims image.Point, title, txt string) image.Point {
-	for {
-		e, ok := w.inp.Next(ctx, Up, Down)
-		if !ok {
-			break
-		}
-		switch e.Button {
-		case Up:
-			if e.Pressed {
-				w.scroll -= w.txtclip / 2
-			}
-		case Down:
-			if e.Pressed {
-				w.scroll += w.txtclip / 2
-			}
-		}
-	}
-	const btnMargin = 4
-	const boxMargin = 6
-
-	op.ColorOp(ops, color.NRGBA{A: theme.overlayMask})
-
-	wbbg := assets.WarningBoxBg
-	wbout := assets.WarningBoxBorder
-	ptop, pend, pbottom, pstart := wbbg.Padding()
-	r := image.Rectangle{
-		Min: image.Pt(pstart+boxMargin, ptop+boxMargin),
-		Max: image.Pt(dims.X-pend-boxMargin, dims.Y-pbottom-boxMargin),
-	}
-	box := wbbg.Add(ops, r, true)
-	op.ColorOp(ops, th.Background)
-	wbout.Add(ops, r, true)
-	op.ColorOp(ops, th.Text)
-
-	btnOff := assets.NavBtnPrimary.Bounds().Dx() + btnMargin
-	titlesz := widget.Labelwf(ops.Begin(), ctx.Styles.warning, dims.X-btnOff*2, th.Text, "%s", strings.ToTitle(title))
-	titlew := ops.End()
-	op.Position(ops, titlew, image.Pt((dims.X-titlesz.X)/2, r.Min.Y))
-
-	bodyClip := image.Rectangle{
-		Min: image.Pt(pstart+boxMargin, ptop+titlesz.Y),
-		Max: image.Pt(dims.X-btnOff, dims.Y-pbottom-boxMargin),
-	}
-	bodysz := widget.Labelwf(ops.Begin(), ctx.Styles.body, bodyClip.Dx(), th.Text, "%s", txt)
-	body := ops.End()
-	innerCtx := ops.Begin()
-	w.txtclip = bodyClip.Dy()
-	maxScroll := bodysz.Y - (bodyClip.Dy() - 2*scrollFadeDist)
-	if w.scroll > maxScroll {
-		w.scroll = maxScroll
-	}
-	if w.scroll < 0 {
-		w.scroll = 0
-	}
-	op.Position(innerCtx, body, image.Pt(bodyClip.Min.X, bodyClip.Min.Y+scrollFadeDist-w.scroll))
-	fadeClip(ops, ops.End(), image.Rectangle(bodyClip))
-
-	return box.Bounds().Size()
-}
-
-func (s *ConfirmWarningScreen) Layout(ctx *Context, ops op.Ctx, th *Colors, dims image.Point) ConfirmResult {
-	var progress float32
-	for {
-		progress = s.confirm.Progress(ctx)
-		if progress == 1 {
-			return ConfirmYes
-		}
-		e, ok := s.inp.Next(ctx, Button3, Button1)
-		if !ok {
-			break
-		}
-		switch e.Button {
-		case Button1:
-			if s.inp.Clicked(e.Button) {
-				return ConfirmNo
-			}
-		case Button3:
-			if e.Pressed {
-				s.confirm.Start(ctx, confirmDelay)
-			} else {
-				s.confirm = ConfirmDelay{}
-			}
-		}
-	}
-	s.warning.Layout(ctx, ops, th, dims, s.Title, s.Body)
-	layoutNavigation(&s.inp, ops, th, dims, []NavButton{
-		{Button: Button1, Style: StyleSecondary, Icon: assets.IconBack},
-		{Button: Button3, Style: StylePrimary, Icon: s.Icon, Progress: progress},
-	}...)
-	return ConfirmNone
-}
-
 type ProgressImage struct {
 	Progress float32
 	Src      image.RGBA64Image
@@ -429,19 +262,6 @@ func NewErrorScreen(err error) *ErrorScreen {
 			Title: "Error",
 			Body:  err.Error(),
 		}
-	}
-}
-
-func confirmWarning(ctx *Context, ops op.Ctx, th *Colors, w *ConfirmWarningScreen) bool {
-	for {
-		dims := ctx.Platform.DisplaySize()
-		switch w.Layout(ctx, ops, th, dims) {
-		case ConfirmYes:
-			return true
-		case ConfirmNo:
-			return false
-		}
-		ctx.Frame()
 	}
 }
 
