408c07d0ae58fdd5f5f226a7836be51ecb2d0223
cmyk <schelob@protonmail.com>
2025-12-06 21:29:10 +0100

    refactor(gui): move main flow into screen_main

--- gui/gui.go
+++ gui/gui.go
@@ -169,12 +169,6 @@ func (t *InputTracker) Clicked(b Button) bool {
 	return c
 }
 
-type program int
-
-const (
-	backupWallet program = iota
-)
-
 type richText struct {
 	Y int
 }
@@ -589,108 +583,6 @@ func NewErrorScreen(err error) *ErrorScreen {
 			Body:  err.Error(),
 		}
 	}
-}
-
-func mainFlow(ctx *Context, ops op.Ctx) {
-	var page program
-	inp := new(InputTracker)
-	for {
-		dims := ctx.Platform.DisplaySize()
-	events:
-		for {
-			e, ok := inp.Next(ctx, Button3, Center, Left, Right)
-			if !ok {
-				break
-			}
-			switch e.Button {
-			case Button3, Center:
-				if !inp.Clicked(e.Button) {
-					break
-				}
-				ws := &ConfirmWarningScreen{
-					Title: "Remove SD card",
-					Body:  "Remove SD card to continue.\n\nHold button to ignore this warning.",
-					Icon:  assets.IconRight,
-				}
-				th := mainScreenTheme(page)
-			loop:
-				for !ctx.EmptySDSlot {
-					res := ws.Layout(ctx, ops.Begin(), th, dims)
-					dialog := ops.End()
-					switch res {
-					case ConfirmYes:
-						break loop
-					case ConfirmNo:
-						continue events
-					}
-					drawMainScreen(ctx, ops, dims, page)
-					dialog.Add(ops)
-					ctx.Frame()
-				}
-				ctx.EmptySDSlot = true
-				switch page {
-				case backupWallet:
-					backupWalletFlow(ctx, ops, th)
-				}
-			case Left:
-				if !e.Pressed {
-					break
-				}
-				page--
-				if page < 0 {
-					page = backupWallet
-				}
-			case Right:
-				if !e.Pressed {
-					break
-				}
-				page++
-				if page > backupWallet {
-					page = 0
-				}
-			}
-		}
-		drawMainScreen(ctx, ops, dims, page)
-		layoutNavigation(inp, ops, mainScreenTheme(page), dims, []NavButton{
-			{Button: Button3, Style: StylePrimary, Icon: assets.IconCheckmark},
-		}...)
-		ctx.Frame()
-	}
-}
-
-func mainScreenTheme(page program) *Colors {
-	switch page {
-	case backupWallet:
-		return &descriptorTheme
-	default:
-		panic("invalid page")
-	}
-}
-
-func drawMainScreen(ctx *Context, ops op.Ctx, dims image.Point, page program) {
-	var th *Colors
-	var title string
-	th = mainScreenTheme(page)
-	switch page {
-	case backupWallet:
-		title = "SeedEtcher Backup"
-	}
-	op.ColorOp(ops, th.Background)
-
-	layoutTitle(ctx, ops, dims.X, th.Text, "%s", title)
-
-	r := layout.Rectangle{Max: dims}
-	sz := layoutMainPage(ops.Begin(), th, dims.X, page)
-	op.Position(ops, ops.End(), r.Center(sz))
-
-	sz = layoutMainPager(ops.Begin(), th, page)
-	_, footer := r.CutBottom(leadingSize)
-	op.Position(ops, ops.End(), footer.Center(sz))
-
-	versz := widget.Labelwf(ops.Begin(), ctx.Styles.debug, 100, th.Text, "%s", ctx.Version)
-	op.Position(ops, ops.End(), r.SE(versz.Add(image.Pt(4, 0))))
-	shsz := widget.Labelwf(ops.Begin(), ctx.Styles.debug, 100, th.Text, "SeedEtcher")
-	op.Position(ops, ops.End(), r.SW(shsz).Add(image.Pt(3, 0)))
 }
 
 func confirmWarning(ctx *Context, ops op.Ctx, th *Colors, w *ConfirmWarningScreen) bool {
