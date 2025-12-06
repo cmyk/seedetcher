9ce8fc97abbe251420b8600977dca6937fcfc995
cmyk <schelob@protonmail.com>
2025-12-06 21:21:40 +0100

    refactor(gui): move shared scroll/seed helpers

--- gui/gui.go
+++ gui/gui.go
@@ -591,41 +591,6 @@ func NewErrorScreen(err error) *ErrorScreen {
 	}
 }
 
-func isEmptyMnemonic(m bip39.Mnemonic) bool {
-	for _, w := range m {
-		if w != -1 {
-			return false
-		}
-	}
-	return true
-}
-
-func emptyMnemonic(nwords int) bip39.Mnemonic {
-	m := make(bip39.Mnemonic, nwords)
-	for i := range m {
-		m[i] = -1
-	}
-	return m
-}
-
-const scrollFadeDist = 16
-
-func fadeClip(ops op.Ctx, w op.CallOp, r image.Rectangle) {
-	op.ParamImageOp(ops, scrollMask, true, r, nil, nil)
-	op.Position(ops, w, image.Pt(0, 0))
-}
-
-var scrollMask = op.RegisterParameterizedImage(func(args op.ImageArguments, x, y int) color.RGBA64 {
-	alpha := 0xffff
-	if d := y - args.Bounds.Min.Y; d < scrollFadeDist {
-		alpha = 0xffff * d / scrollFadeDist
-	} else if d := args.Bounds.Max.Y - y; d < scrollFadeDist {
-		alpha = 0xffff * d / scrollFadeDist
-	}
-	a16 := uint16(alpha)
-	return color.RGBA64{A: a16}
-})
-
 type ChoiceScreen struct {
 	Title   string
 	Lead    string
