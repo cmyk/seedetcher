49ae120dc83284dd21b178e52295e904b40e4198
cmyk <schelob@protonmail.com>
2026-03-12 16:29:00 +0100

    test(scene): add compact 2of3 snapshot coverage

--- printer/scene_snapshot_test.go
+++ printer/scene_snapshot_test.go
@@ -13,23 +13,33 @@ import (
 func TestWalletSceneSnapshots(t *testing.T) {
 	cases := []struct {
 		wallet     string
+		compact2of3 bool
 		wantScenes int
 		wantHash   string
 	}{
 		{
-			wallet:     "singlesig",
-			wantScenes: 1,
-			wantHash:   "17e9fd052839062ee7b7c2ecc276cab80971ec6d06005de6d38a290bdb0a854d",
+			wallet:      "singlesig",
+			compact2of3: false,
+			wantScenes:  1,
+			wantHash:    "17e9fd052839062ee7b7c2ecc276cab80971ec6d06005de6d38a290bdb0a854d",
 		},
 		{
-			wallet:     "multisig-mainnet-2of3",
-			wantScenes: 6,
-			wantHash:   "3c7656cfc0145a4cd7f1c7cbe9b3f8a0ab2c432f50c9faccf10f340f2bdb2b1e",
+			wallet:      "multisig-mainnet-2of3",
+			compact2of3: false,
+			wantScenes:  6,
+			wantHash:    "3c7656cfc0145a4cd7f1c7cbe9b3f8a0ab2c432f50c9faccf10f340f2bdb2b1e",
 		},
 		{
-			wallet:     "multisig-3of5",
-			wantScenes: 10,
-			wantHash:   "2878e41d532630ee0106da75d3300aad4a7e5737fb31dfb5b085ed4dfc529f93",
+			wallet:      "multisig-3of5",
+			compact2of3: false,
+			wantScenes:  10,
+			wantHash:    "2878e41d532630ee0106da75d3300aad4a7e5737fb31dfb5b085ed4dfc529f93",
+		},
+		{
+			wallet:      "multisig",
+			compact2of3: true,
+			wantScenes:  3,
+			wantHash:    "ca3b8c934bc82445769b21417a827e0c9b660cc42c0e6db35d2c672333f1e1c0",
 		},
 	}
 
@@ -38,7 +48,7 @@ func TestWalletSceneSnapshots(t *testing.T) {
 		t.Run(tc.wallet, func(t *testing.T) {
 			SetWalletLabel(DefaultWalletLabel)
 			SetDescriptorQRSize(80.0)
-			SetCompactDescriptor2of3Enabled(false)
+			SetCompactDescriptor2of3Enabled(tc.compact2of3)
 
 			cfg, ok := testutils.WalletConfigs[tc.wallet]
 			if !ok {
